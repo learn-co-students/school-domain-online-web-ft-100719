72e8e42372e7d38b83392e1c3c8ca82b41f42060
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-10-13 06:50:31 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School 
+
+    def initialize(student, roster = {})
+    @student = student
+    @roster = roster
+    
+    end
+
+    def roster
+        @roster
+    end
+    
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map do |key, value|
+        @roster[key] = value.sort
+        end
+        
+        @roster
+    end
+
+end
