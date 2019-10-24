ece52c373eca557dad80be91cce33afc2cd4ba3d
Michael Calhoun <iengineersoftware@gmail.com>
2019-10-24 21:47:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    sorted = {}
+    roster.each do |key, value|
+      sorted[key] = value.sort
+    end
+    sorted
+  end
+end
+
+
+
+    # if !roster[grade]
+    #   roster[grade] = []
+    # end
+    # https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Operators
+    # learned can ignore all that if stuff and just use the shorthand ||= instead
