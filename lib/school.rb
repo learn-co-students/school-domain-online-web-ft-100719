559b842303bb7f41e8dac7a383de6ac4ea7cd99a
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-07 20:51:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+  def add_student(name,grade)
+    if !@roster.key?(grade)
+      @roster[grade]=[]
+    end
+    @roster[grade]<<name
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each do |grade,student|
+      student.sort!
+    end
+  end
+end
