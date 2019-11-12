7b57e23455e1bfd65012b0e86648e87496e01988
William Pannell <lpann5@gmail.com>
2019-11-12 15:57:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+#require 'pry'
+class School
+  attr_accessor :sname, :grade, :student
+  attr_reader :sname, :grade, :student
+  def initialize(sname)
+    @sname=sname
+    @ros = {}
+  end
+  def roster()
+    @ros
+  end
+  def add_student(student, grade)
+    if @ros[grade]==nil
+    @ros[grade] = []
+    end
+    @ros[grade] << student
+  end
+  def grade(grade)
+    @ros[grade]
+  end
+  def sort()
+    @ros[grade].sort_by {|k, v| v}
+    
+  end
+end
+#binding.pry
